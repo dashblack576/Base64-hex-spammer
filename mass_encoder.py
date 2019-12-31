b60d833f1553e9c0b5ef476a853a9505d3fdf363
dashblack576 <59393700+dashblack576@users.noreply.github.com>
2019-12-31 10:16:10 -0500

    uploading latest version
    added in decoding functions as well as true loading.

--- mass_encoder.py
+++ mass_encoder.py
@@ -1,31 +1,27 @@
 #! /usr/bin/local/python3/
 
-# arbitrary program to encode, reencode in a loop of base64 and hex
-
 import base64
 import time 
 import binascii
 import os
 
+
+
 target_str = input("Enter the thing that you want to be encoded\n>>>")
-how_many = int(input("How many times do you want your string to be encoded?\n>>>"))
+how_many = int(input("How many times do you want your string to be encoded? The more encodes you do the longer it will take to encode. This may take a while.\n>>>"))
 
 
-# convert the target string into base64
 def convert_to_64():
     global target_str
     target_str = target_str.encode()
     target_str = base64.b64encode(target_str)
     target_str = target_str.decode()
 
-# convert the target string into hex
 def convert_to_hex():
     global target_str
     target_str = target_str.encode()
     target_str = binascii.hexlify(target_str)
     target_str = target_str.decode()
-
-
 
 def final():
     global target_str
@@ -34,11 +30,6 @@ def final():
 wait = True
 time_until_conv = 0
 x = 0
-
-while not x==how_many: 
-    convert_to_64()
-    convert_to_hex()
-    x += 1
 
 while wait ==True:
     os.system('clear')
@@ -54,15 +45,46 @@ while wait ==True:
     print("loading...")
     time.sleep(1)
     os.system('clear')
+    convert_to_64()
+    convert_to_hex()
     time_until_conv += 1
 
-    if time_until_conv ==5:
+    if time_until_conv ==how_many:
         final()
         wait = False
     else:
         wait = True
 
 
+def base64_decode():
+    global easy
+    easy = easy.encode()
+    easy = base64.b64decode(easy)
+    easy = easy.decode()
+ 
+ def hex_decode():
+    global easy
+    easy = easy.encode()
+    easy = binascii.unhexlify(easy)
+    easy = easy.decode()
+ 
+def final():
+    global easy
+    print(easy)
+
+
+
+
+wait2 = True
+
+while wait 2 ==True 
+
+decode? = input("Time to decode! If you don't want to just hit enter.")
+
+if decode? =="":
+    wait2 = False
+
+else
 
 
 
